22bf50e2129d58691deb78f35ec37b1d59268282
braye98 <jonorebra@gmail.com>
2019-09-01 19:03:32 -0400

    add jquaery validate

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,3 +1,10 @@
+Recaptcha.configure do |config|
+  config.site_key   = '6LffEZIUAAAAAAJL9ZaPdCDPBGErMYrVomGHdPUR'
+  config.secret_key = '6LffEZIUAAAAAFniMcsduvZ3P6LZLa5hsnmkCMCL'
+  # Uncomment the following line if you are using a proxy server:
+  # config.proxy = 'http://myproxy.com.au:8080'
+end
+
 # Recaptcha.configure do |config|
 # 	config.site_key = Rails.application.secrets.site_key
 # 	config.secret_key = Rails.application.secrets.secret_key
